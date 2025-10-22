75d398e68e17acc35826ac830cf78542682ab48d
loki021004 <lokeshmpt2020@gmail.com>
2025-10-22 13:02:57 +0530

    deploy data

--- backend/app.js
+++ backend/app.js
@@ -24,13 +24,6 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-// Handle preflight requests
-app.options("*", cors({
-  origin: allowedOrigins,
-  credentials: true,
-  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
 
 app.use(express.json());
 app.use(cookieParser());
